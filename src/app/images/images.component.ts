dd1b17a146dc8d44460aad8f88c40b1425544caf
snakierten96 <neil.lunn1@gmail.com>
2016-12-05 22:54:30 +0000

    include web-animations polyfill

--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -17,7 +17,7 @@ import { ImgHelperDirective } from './img-helper.directive';
     trigger('crossFade',[
       state('pre',  style({ opacity: 1 })),
       state('fade', style({ opacity: 0 })),
-      transition('pre => fade', animate('500ms ease-out'))
+      transition('pre => fade', animate('1s ease-out'))
     ])
   ]
 })
